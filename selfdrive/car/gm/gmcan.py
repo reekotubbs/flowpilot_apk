3fb05e9fa1cf8d5c6c3aa1477b52f6aeac69d099
reekotubbs <reekotubbs@hotmail.com>
2024-12-28 10:03:30 -0800

    Add Volt_CC Fingerprint Cont.
    Update gm\carcontroller.py and gm\gmcan.py

--- selfdrive/car/gm/gmcan.py
+++ selfdrive/car/gm/gmcan.py
@@ -1,6 +1,14 @@
+#####Begin from opgm-build
+'''from cereal import log'''
+from common.conversions import Conversions as CV
+from common.realtime import DT_CTRL
+#####End from opgm-build
+    
 from selfdrive.car import make_can_msg
-from selfdrive.car.gm.values import CAR
 
+#####Begin from opgm-build
+from selfdrive.car.gm.values import CAR, CruiseButtons, CanBus
+#####End from opgm-build
 
 def create_buttons(packer, bus, idx, button):
   values = {
@@ -103,13 +111,36 @@ def create_friction_brake_command(packer, bus, apply_brake, idx, enabled, near_s
 
 
 def create_acc_dashboard_command(packer, bus, enabled, target_speed_kph, lead_car_in_sight, fcw):
+
+'''#####Begin from opgm-build
+def create_acc_dashboard_command(packer, bus, enabled, personality, target_speed_kph, lead_car_in_sight, fcw):
+#####End from opgm-build'''
+
   target_speed = min(target_speed_kph, 255)
+
+  '''#####Begin from opgm-build
+  if not enabled:
+    gap = 0
+  elif personality == log.LongitudinalPersonality.aggressive:
+    gap = 1
+  elif personality == log.LongitudinalPersonality.standard:
+    gap = 2
+  elif personality == log.LongitudinalPersonality.relaxed:
+    gap = 3
+  else:
+    gap = 3
+  #####End from opgm-build'''
 
   values = {
     "ACCAlwaysOne": 1,
     "ACCResumeButton": 0,
     "ACCSpeedSetpoint": target_speed,
     "ACCGapLevel": 3 * enabled,  # 3 "far", 0 "inactive"
+    
+    '''#####Begin from opgm-build
+    "ACCGapLevel": gap,
+    #####End from opgm-build'''
+    
     "ACCCmdActive": enabled,
     "ACCAlwaysOne2": 1,
     "ACCLeadCar": lead_car_in_sight,
@@ -171,3 +202,50 @@ def create_lka_icon_command(bus, active, critical, steer):
   else:
     dat = b"\x00\x00\x00"
   return make_can_msg(0x104c006c, dat, bus)
+  
+#####Begin from opgm-build
+def create_gm_cc_spam_command(packer, controller, CS, actuators):
+  if controller.params_.get_bool("IsMetric"):
+    _CV = CV.MS_TO_KPH
+    RATE_UP_MAX = 0.04
+    RATE_DOWN_MAX = 0.04
+  else:
+    _CV = CV.MS_TO_MPH
+    RATE_UP_MAX = 0.2
+    RATE_DOWN_MAX = 0.2
+
+  accel = actuators.accel * _CV  # m/s/s to mph/s
+  speedSetPoint = int(round(CS.out.cruiseState.speed * _CV))
+
+  cruiseBtn = CruiseButtons.INIT
+  if speedSetPoint == CS.CP.minEnableSpeed and accel < -1:
+    cruiseBtn = CruiseButtons.CANCEL
+    controller.apply_speed = 0
+    rate = 0.04
+  elif accel < 0:
+    cruiseBtn = CruiseButtons.DECEL_SET
+    if speedSetPoint > (CS.out.vEgo * _CV) + 3.0:  # If accel is changing directions, bring set speed to current speed as fast as possible
+      rate = RATE_DOWN_MAX
+    else:
+      rate = max(-1 / accel, RATE_DOWN_MAX)
+    controller.apply_speed = speedSetPoint - 1
+  elif accel > 0:
+    cruiseBtn = CruiseButtons.RES_ACCEL
+    if speedSetPoint < (CS.out.vEgo * _CV) - 3.0:
+      rate = RATE_UP_MAX
+    else:
+      rate = max(1 / accel, RATE_UP_MAX)
+    controller.apply_speed = speedSetPoint + 1
+  else:
+    controller.apply_speed = speedSetPoint
+    rate = float('inf')
+
+  # Check rlogs closely - our message shouldn't show up on the pt bus for us
+  # Or bus 2, since we're forwarding... but I think it does
+  if (cruiseBtn != CruiseButtons.INIT) and ((controller.frame - controller.last_button_frame) * DT_CTRL > rate):
+    controller.last_button_frame = controller.frame
+    idx = (CS.buttons_counter + 1) % 4  # Need to predict the next idx for '22-23 EUV
+    return [create_buttons(packer, CanBus.POWERTRAIN, idx, cruiseBtn)]
+  else:
+    return []
+#####End from opgm-build
